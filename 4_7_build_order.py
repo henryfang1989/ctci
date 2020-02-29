b31410012be45ba590b61de4847d619a263fdf09
Henry Fang <fanghuangwei@gmail.com>
2020-02-29 20:41:34 +0000

    add 4.7

---
+++ 4_7_build_order.py
@@ -0,0 +1,10 @@
+# question: You are given a list of projects and
+# list of dependencies (which is a lost of pairs
+# of projects, where the second projects dependent
+# on the first prject). All of a project's  dependencies
+# must be built before the project is. Find a build
+# order that will allow the projects to be built. 
+# If there is no valid build order, return an error.
+
+def build(projects):
+    pass
